ddfe360fb664a505313c165d498ddb96364e5b76
vigneshkumarme47 <vigneshkumarme47@gmail.com>
2017-06-16 14:17:34 +0530

    Create print social network.c

---
+++ print social network.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char l,a;
+    scanf("%c",&l);
+    if(l=='t'||l=='T')
+    
+    printf ("twitter");
+    else if(l=='w'||l=='W')
+    
+            printf ("whatsapp");
+            else if(l=='f'||l=='F')
+            
+                printf ("facebook");
+                else
+                printf ("undefined");
+            
+            
+         
+    return 0;
+    
+ }
