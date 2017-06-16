e79b8671ef41eedd6158510c3793006e43fa6a4c
vigneshkumarme47 <vigneshkumarme47@gmail.com>
2017-06-16 14:07:46 +0530

    Create reverseorderprintmultiplesofXfromMtoN.c

---
+++ reverseorderprintmultiplesofXfromMtoN.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b,c,i,k=0;
+    scanf("%d \n %d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if(i%2!=0)
+        k=k+i;
+    }
+    printf("%d",k);
+
+}
