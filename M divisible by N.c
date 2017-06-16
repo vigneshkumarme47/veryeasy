d5937629340c698962d9c0ca715c53c48fdd613a
vigneshkumarme47 <vigneshkumarme47@gmail.com>
2017-06-16 14:21:13 +0530

    Create M divisible by N.c

---
+++ M divisible by N.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c,fuck,ombu;
+    scanf("%d \n",&fuck);
+    scanf("%d",&ombu);
+    if(fuck%ombu==0)
+    printf("YES");
+    else
+    printf("NO");
+}    
